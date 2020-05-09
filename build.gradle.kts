45ede5d7d6d8e7398254768f0cf1010c5e8c4f48
Mauricio Rojas <mauricio.rojas@thoughtworks.com>
2020-05-09 17:46:37 -0400

    Initial project Kotlin from Spring Boot.

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.2.7.RELEASE"
+	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	kotlin("jvm") version "1.3.72"
+	kotlin("plugin.spring") version "1.3.72"
+	kotlin("plugin.jpa") version "1.3.72"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-mustache")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	// jackson-module-kotlin adds support for serialization/deserialization of Kotlin classes and data classes.
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	// kotlin-reflect is Kotlin reflection library.
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	// kotlin-stdlib-jdk8 is the Java 8 variant of Kotlin standard library.
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	runtimeOnly("org.springframework.boot:spring-boot-devtools")
+	runtimeOnly("com.h2database:h2")
+	
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
---
+++ src/main/kotlin/com/example/blog/BlogApplication.kt
@@ -0,0 +1,14 @@
+package com.example.blog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.boot.Banner;
+
+@SpringBootApplication
+class BlogApplication
+
+fun main(args: Array<String>) {
+	runApplication<BlogApplication>(*args){
+		setBannerMode(Banner.Mode.OFF)
+	}
+}
